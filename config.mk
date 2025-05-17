847e0aeff112f0ed791c49de4390e4d8136f7e39
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-05-17 21:29:41 +0200

    update makefiles

--- config.mk
+++ config.mk
@@ -3,11 +3,22 @@ ARR			  = ar
 
 VESRION_C     = 11
 
-PATH_SRC 	      = src
-PATH_INCLUDE      = include
-PATH_EXAMPLES	  = example
+PATH_SRC 	      		= src
+PATH_INCLUDE      		= include
+PATH_EXAMPLES	  		= example
+PATH_SimpleOfuscator 	= ./lib/SimpleOfuscator
+PATH_LZ77_c		  		= $(PATH_SimpleOfuscator)/LZ77_c
 
-INCLUDE_FLAGS = -I. -I$(PATH_INCLUDE)
+LINKER_FLAGS  	    = 									\
+	-L.							-lGetCommandOutputC		\
+	-L$(PATH_SimpleOfuscator)	-lSimpleOfuscator		\
+	-L$(PATH_LZ77_c) 			-lLZ77_c				\
+
+
+INCLUDE_FLAGS = -I. 									\
+				-I$(PATH_INCLUDE)						\
+				-I$(PATH_SimpleOfuscator)/include		\
+				-I$(PATH_LZ77_c)/include
 GLOBAL_CFLAGS = -std=c$(VESRION_C) $(INCLUDE_FLAGS) -masm=intel \
 				-D_ExceptionHandler -fdiagnostics-color=always $(DEBUG_LINUX)
 
@@ -15,5 +26,4 @@ CFLAGS 		  =  $(GLOBAL_CFLAGS) -O3 -Wno-unused-parameter \
 				-Wno-implicit-fallthrough -Wno-type-limits  \
 				-Wno-unused-variable -Wno-pointer-sign
 ARR_FLAGS     = -rc
-
 OBJECTS = 	GetCommandOutputC.o
--- general.mk
+++ general.mk
@@ -2,7 +2,7 @@ include config.mk
 
 all: $(TARGET).a
 	ar -t $^
-	gcc $(CFLAGS) $(INCLUDE_FLAGS) $(PATH_EXAMPLES)/code.c -L. -lGetCommandOutputC -o code.$(EXTENSION)
+	gcc $(CFLAGS) $(INCLUDE_FLAGS) $(PATH_EXAMPLES)/code.c $(LINKER_FLAGS) -o code.$(EXTENSION)
 
 $(TARGET).a: $(OBJECTS)
 	$(ARR) $(ARR_FLAGS) $@ $^
@@ -13,10 +13,12 @@ GetCommandOutputC.o: $(PATH_SRC)/GetCommandOutputC.c
 
 cleanobj:
 	$(RM) $(RMFLAGS) $(OBJECTS)
+	$(MAKE) -C ./$(PATH_SimpleOfuscator) -f $(MAKE_NAME) cleanobj
 
 cleanall: cleanobj
-	$(RM) $(RMFLAGS) $(TARGET).o $(TARGET).a \
-	$(TARGET_DEBUG).a
+	$(RM) $(RMFLAGS) *.o $(TARGET).a \
+	$(TARGET_DEBUG).a *.$(EXTENSION) 
+	$(MAKE) -C ./$(PATH_SimpleOfuscator) -f $(MAKE_NAME) cleanall
 
 .SILENT: clean cleanobj cleanall
 .IGNORE: cleanobj cleanall
